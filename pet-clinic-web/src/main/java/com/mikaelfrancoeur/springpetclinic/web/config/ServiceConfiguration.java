18dbe1577e74fa1daf8357e94fa53bc7eddd238f
Mikaël Francoeur <mikael@mikaelfrancoeur.com>
2020-05-05 23:39:35 -0400

    Implement Spring configuration for services. Closes #25.

---
+++ pet-clinic-web/src/main/java/com/mikaelfrancoeur/springpetclinic/web/config/ServiceConfiguration.java
@@ -0,0 +1,32 @@
+package com.mikaelfrancoeur.springpetclinic.web.config;
+
+import com.mikaelfrancoeur.springpetclinic.services.OwnerService;
+import com.mikaelfrancoeur.springpetclinic.services.PetService;
+import com.mikaelfrancoeur.springpetclinic.services.VetService;
+import com.mikaelfrancoeur.springpetclinic.services.map.OwnerServiceMap;
+import com.mikaelfrancoeur.springpetclinic.services.map.PetServiceMap;
+import com.mikaelfrancoeur.springpetclinic.services.map.VetServiceMap;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class ServiceConfiguration {
+    @Bean
+    @Profile({"map", "default"})
+    OwnerService ownerService() {
+        return new OwnerServiceMap();
+    }
+
+    @Bean
+    @Profile({"map", "default"})
+    PetService petService() {
+        return new PetServiceMap();
+    }
+
+    @Bean
+    @Profile({"map", "default"})
+    VetService vetService() {
+        return new VetServiceMap();
+    }
+}
