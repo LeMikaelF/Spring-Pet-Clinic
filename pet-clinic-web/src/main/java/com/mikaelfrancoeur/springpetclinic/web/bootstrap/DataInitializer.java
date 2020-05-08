1300aa2a2f19a89031f5529ab664eee1943d219b
Mikaël Francoeur <mikael@mikaelfrancoeur.com>
2020-05-07 20:07:55 -0400

    Add bootstrap pet data. Closes #35.

--- pet-clinic-web/src/main/java/com/mikaelfrancoeur/springpetclinic/web/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/com/mikaelfrancoeur/springpetclinic/web/bootstrap/DataInitializer.java
@@ -1,47 +1,98 @@
 package com.mikaelfrancoeur.springpetclinic.web.bootstrap;
 
-import com.mikaelfrancoeur.springpetclinic.model.Owner;
-import com.mikaelfrancoeur.springpetclinic.model.Vet;
+import com.mikaelfrancoeur.springpetclinic.model.*;
 import com.mikaelfrancoeur.springpetclinic.services.OwnerService;
+import com.mikaelfrancoeur.springpetclinic.services.PetTypeService;
 import com.mikaelfrancoeur.springpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataInitializer(OwnerService ownerService, VetService vetService) {
+    public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        final PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        final PetType savedCatPetType = petTypeService.save(cat);
+
+        Specialty radiology = new Specialty();
+        radiology.setDescription("Radiology");
+
+        Specialty surgery = new Specialty();
+        surgery.setDescription("Surgery");
+
+        Specialty dentistry = new Specialty();
+        dentistry.setDescription("dentistry");
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("1231231234");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        mikesPet.setPetType(savedDogPetType);
+        owner1.getPets().add(mikesPet);
+
+        ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner2.setAddress("123 Brickerel");
+        owner2.setCity("Miami");
+        owner2.setTelephone("1231231234");
+
+        Pet fionasCat = new Pet();
+        fionasCat.setName("Just Cat");
+        fionasCat.setOwner(owner2);
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setPetType(savedCatPetType);
+        owner2.getPets().add(fionasCat);
+
+        ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+
+        System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
+        vetService.save(vet1);
+
         Vet vet2 = new Vet();
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
-        ownerService.save(owner1);
-        ownerService.save(owner2);
-        vetService.save(vet1);
         vetService.save(vet2);
-        System.out.println("Saved owners and vets in DataInitializer");
 
-        System.out.println(ownerService.findById(1L));
+        System.out.println("Loaded Vets....");
     }
 }
